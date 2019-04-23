9ac8a352ad0a1c9cb644db8299971560efccb7e6
IPxK <p.xaymountry@realitychecksystems.com>
2019-04-23 08:10:38 -0700

    SMS

--- MyFirstProject/lambda.js
+++ MyFirstProject/lambda.js
@@ -1,4 +1,39 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+    callback(null, { "message": "Successfully executed" });
+}
+
+
+exports.handler = function (event, context, callback) {
+    sns.publish({
+        Message: message,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                DataType: 'String',
+                StringValue: 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                DataType: 'String',
+                StringValue: '@sender'
+            },
+        },
+        PhoneNumber: receiver
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['message'];
+
+    console.log("Sending message", message, "to receiver", receiver);
+
+    callback(null, 'Successfully executed');
 }
